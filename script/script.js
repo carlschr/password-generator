d3b82f1f4439428bdc5556d4940ec4d29976a764
carlschr <cjcarlson136@gmail.com>
2020-10-17 15:58:10 -0500

    Add user input for number of characters

--- script/script.js
+++ script/script.js
@@ -12,3 +12,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Returns number of characters chosen by the user if the input was between 8 and 128
+const numOfChars = () => {
+    let passed = false;
+    do {
+        let numChar = prompt("How many characters long would you like your password? (Must be at least 8 characters)");
+        if (typeof +numChar === 'number' && +numChar >= 8 && +numChar < 128) {
+            passed = true;
+            return numChar;
+        } else {
+            alert('Number of characters must be a number between 8 and 128!');
+        };
+    } while (passed === false);
+};
